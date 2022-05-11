01fef095557250875bb4b2dcc71cbe9035be8fb1
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-05-11 20:13:25 -0300

    MovieCard

--- frontend/src/paginas/listas/index.tsx
+++ frontend/src/paginas/listas/index.tsx
@@ -1,8 +1,34 @@
+import MovieCard from 'components/moviecard';
 import Paginacao from 'components/paginacao';
 
 function listas(){
     return(
-        <Paginacao />
+        <>
+            <Paginacao  />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+  
+            
+        </>
     );
 }
 
