a89c86222fc4a367315d06ba6d004e6df1be2f54
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-05-11 23:24:59 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,46 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePK id = new ScorePK(); 
+	private Double value;
+	
+	public Score() {
+		
+	}
+	
+	// Metodo que associa um Score a um Movie
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	// Metodo que associa um Score a um User
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+	
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	
+}
