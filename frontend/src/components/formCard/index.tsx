c33489b238b602481e787ee906bde365c904863a
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-05-13 10:33:11 -0300

    useParams

---
+++ frontend/src/components/formCard/index.tsx
@@ -0,0 +1,64 @@
+import './styles.css';
+import { Link }   from "react-router-dom";
+import { Movie } from 'types/movie';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
+
+type Props = {
+    movieId : string;
+}
+
+function FormCard({ movieId } : Props){
+
+
+    //OBJETO MOCKADO. DEVE-SE FAZER O USO DE PROPS
+    //const movie = {
+    //    id: 1,
+    //    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    //    title: "The Witcher",
+    //    count: 2,
+    //    score: 4.5
+    //};
+
+   const [movie,setMovie] = useState<Movie>();
+
+   useEffect(() => {
+       axios.get(`${BASE_URL}/movies/${movieId}`).then(response => {
+           setMovie(response.data);
+       })
+   }, [movieId]);
+
+    return(
+    <div className="dsmovie-form-container">
+        <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.title} />
+        <div className="dsmovie-card-bottom-container">
+            <h3>{movie?.title}</h3>
+            <form className="dsmovie-form">
+               <div className="form-group dsmovie-form-group">
+                    <label htmlFor="email">Informe seu email</label>
+                    <input type="email" className="form-control" id="email" />
+                </div>
+                <div className="form-group dsmovie-form-group">
+                    <label htmlFor="score">Informe sua avaliação</label>
+                    <select className="form-control" id="score">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+                <div className="dsmovie-form-btn-container">
+                    <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                </div>
+            </form >
+            <Link to="/">
+                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+            </Link>
+        </div >
+    </div >
+    );
+}
+
+export default FormCard;
